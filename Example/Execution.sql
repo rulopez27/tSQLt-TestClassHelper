26486a7f8d921b9af3f04120831727a475f57845
rulopez <ruben_guille27@hotmail.com>
2021-04-30 16:32:54 -0700

    Added example execution script

---
+++ Example/Execution.sql
@@ -0,0 +1,17 @@
+--[TODO: Uncomment the next line and change <DATABASE_NAME> to your database name
+--USE <DATABASE_NAME>
+GO
+
+--[TODO: Set @OcjectToWriteTest as the stored procedure/function name you like to get the test class
+DECLARE @ObjectToWriteTest VARCHAR(250) = ''
+
+DECLARE @filteredQueries TestClassFilteredQueries
+
+--[TODO: Uncomment this if you are willing to use filtered queries]
+--insert into @filteredQueries(TableName, Filtered, FilterWithValues, SpecificColumnList)
+--values('<TableName>', <Filtered>, <FilterWithValues>, <SpecificColumnList>),
+
+
+EXEC TestClassHelper.spGetTestClass 
+		@ObjectToWriteTest = ObjectToWriteTest,
+		@filteredQueries = @filteredQueries
