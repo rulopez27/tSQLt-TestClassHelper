cf4909179eff23407113ee0c59f859a75248487d
rulopez <ruben_guille27@hotmail.com>
2021-04-30 15:30:55 -0700

    Added TestClassHelperSchemaCreation.sql which creates the TestClassHelper schema and it's functions

--- TestClassHelperSchemaCreation.sql
+++ TestClassHelperSchemaCreation.sql
@@ -1,3 +1,61 @@
+-- ==============================================================
+-- Author:		Ruben Lopez
+-- Create date: 2021-04-30
+-- Description:	Helper script to build a template tSQLt Test Class
+-- ===============================================================
+
+--[TODO: Change [YOUR DATABASE NAME] with the name of your database. I.E. USE AdventureWorks2017
+
+--USE [YOUR DATABASE NAME];
+GO
+
+IF NOT EXISTS(SELECT * FROM sys.schemas WHERE NAME = 'TestClassHelper')
+	BEGIN
+	 	EXEC ('CREATE SCHEMA TestClassHelper');
+	END
+ELSE
+	BEGIN
+		DROP FUNCTION TestClassHelper.fnGetColumnsCorrectFormatForQuery
+		DROP FUNCTION TestClassHelper.fnGetTableFromCSVString
+		EXEC ('DROP SCHEMA TestClassHelper');
+	END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- ====================================================
+-- Author:		Ruben Lopez
+-- Create date: 2021-04-30
+-- Description:	Returns a table from a given CSV string
+-- ====================================================
+CREATE FUNCTION TestClassHelper.fnGetTableFromCSVString
+(
+		@csvStringToSplit nvarchar(max)
+)
+RETURNS 
+	@csvItems TABLE
+	(
+		Item varchar(100)
+	)
+BEGIN
+	DECLARE @item nvarchar(100), @index int;
+	WHILE CHARINDEX(',',@csvStringToSplit) > 0
+		BEGIN
+			SET @index = CHARINDEX(',',@csvStringToSplit)
+			SET @item = LTRIM(RTRIM(SUBSTRING(@csvStringToSplit,1,@index - 1)))
+
+			INSERT INTO @csvItems (Item) VALUES (@item)
+
+			SELECT @csvStringToSplit = SUBSTRING(@csvStringToSplit, @index+1,LEN(@csvStringToSplit)-@index)
+		END
+
+	INSERT INTO @csvItems (Item) VALUES (@csvStringToSplit)
+	RETURN
+END
+GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -76,3 +134,4 @@ BEGIN
 
 	RETURN @formattedColumnForQuery
 END
+GO
